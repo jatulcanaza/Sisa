8bd56a605109be4df45a3049ce97602c1630c44a
jatulcanaza <tulcanazajuan6@gmail.com>
2025-01-26 20:11:46 -0500

    Agrego Información de la página inicio

--- sisa-web/src/app/page.tsx
+++ sisa-web/src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Navbar } from "@/components/Navbar";
+import { Inicio } from "@/components/Inicio";
+import { InformacionInicio } from "@/components/InformacionInicio";
 import Image from "next/image";
 
 export default function Home() {
   return (
 <main className="">
   <Navbar />
+  <Inicio />
+  <InformacionInicio />
 </main>
   );
 }
--- sisa-web/src/components/Navbar/Navbar.tsx
+++ sisa-web/src/components/Navbar/Navbar.tsx
@@ -3,18 +3,26 @@ import Link from 'next/link';
 
 export function Navbar() {
   return (
-    <div className="bg-[#FB6F92]">
-      <div className="grid items-center justify-between md:grid-cols-2 gap-4 md:gap-20 md:px-20">
-        <Image src="/assets/LOGO_SISA.png" alt="Sisa Logo" width={200} height={200} />
-        <div className="flex gap-4 text-center text-white">
+    <div className="bg-[#FB6F92] py-2"> {/* Rellena menos espacio vertical con py-2 */}
+      <div className="container mx-auto flex items-center justify-between px-4 md:px-20">
+        {/* Logo */}
+        <div className="flex-shrink-0">
+          <Image 
+            src="/assets/LOGO_SISA.png" 
+            alt="Sisa Logo" 
+            width={150}  // Reduce el tamaño para un diseño más compacto
+            height={150} 
+          />
+        </div>
+        {/* Enlaces */}
+        <div className="flex gap-4 text-sm md:text-base text-white">
           <Link href="#Inicio" className="hover:text-gray-300">Inicio</Link>
           <Link href="#Nosotros" className="hover:text-gray-300">Nosotros</Link>
           <Link href="#Crear tu ramo" className="hover:text-gray-300">Crear tu ramo</Link>
           <Link href="#Sisa" className="hover:text-gray-300">Sisa</Link>
-          <Link href="#Contactenos" className="hover:text-gray-300">Contactenos</Link>
-        </div> 
+          <Link href="#Contactenos" className="hover:text-gray-300">Contáctenos</Link>
+        </div>
       </div>
     </div>
   );
 }
-
