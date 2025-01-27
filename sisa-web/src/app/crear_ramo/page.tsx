7245df8593ce256b49435bb3e1f2c3695d15b36b
jatulcanaza <tulcanazajuan6@gmail.com>
2025-01-27 09:51:03 -0500

    Agrego Instrucciones en la página crear ramo

--- sisa-web/src/app/crear_ramo/page.tsx
+++ sisa-web/src/app/crear_ramo/page.tsx
@@ -1,6 +1,13 @@
-
+import { Inicio } from "@/components/Inicio";
+import { Instrucciones } from "@/components/Instrucciones";
+import { Navbar } from "@/components/Navbar";
 export default function crear_ramo() {
-  return (
-    <div>crear_ramo</div>
-  )
+    return (
+        <main>
+            <Navbar />
+            <Inicio />
+            <Instrucciones />
+        </main>
+
+    )
 }
