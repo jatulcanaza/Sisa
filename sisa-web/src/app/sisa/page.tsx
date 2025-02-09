4118aeb930e28ab95fd64fe9c8256d78af60a9f0
jatulcanaza <tulcanazajuan6@gmail.com>
2025-02-09 13:12:49 -0500

    Agrego el inicio de la página de Sisa

--- sisa-web/src/app/sisa/page.tsx
+++ sisa-web/src/app/sisa/page.tsx
@@ -1,3 +1,4 @@
+import { Contactenos } from "@/components/Contactenos";
 import { InicioSisa } from "@/components/InicioSisa";
 import { Navbar } from "@/components/Navbar";
 
@@ -6,6 +7,7 @@ export default function sisa() {
    <main>
     <Navbar />
     <InicioSisa />
+    <Contactenos />
    </main>
   )
 }
--- sisa-web/src/components/InicioSisa/InicioSisa.tsx
+++ sisa-web/src/components/InicioSisa/InicioSisa.tsx
@@ -6,7 +6,7 @@ export function InicioSisa() {
         <div
           className="relative bg-cover bg-no-repeat rounded-lg"
           style={{
-            backgroundImage: 'url(/assets/Sisa.png)',
+            backgroundImage: 'url(/assets/sisa_page.jpg)',
             backgroundPosition: 'top', // Centra la imagen en todos los tamaños
             backgroundSize: 'cover',      // La imagen cubre todo el contenedor
             height: '50vh',               // Ajusta la altura del contenedor
@@ -15,9 +15,11 @@ export function InicioSisa() {
           {/* Contenedor del texto alineado */}
           <div className="absolute inset-0 flex flex-col justify-center items-start px-5 md:px-24 text-white">
             <h1 className="text-3xl md:text-6xl font-bold leading-tight">
-              Vive Sisa
+              Sisa 
             </h1>
-
+            <p className="mt-3 md:mt-5 text-lg md:text-3xl">
+            "Tips"
+          </p>
           </div>
         </div>
       </div>
