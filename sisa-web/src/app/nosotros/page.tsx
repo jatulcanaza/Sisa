b905efc59e95b77a24ecbbcf62fc2411a3c92505
jatulcanaza <tulcanazajuan6@gmail.com>
2025-02-08 22:10:08 -0500

    Diseño de la página Nosotros-Inicio

--- sisa-web/src/app/nosotros/page.tsx
+++ sisa-web/src/app/nosotros/page.tsx
@@ -1,9 +1,14 @@
+import { Inicio } from "@/components/Inicio";
+import {InicioNosotros} from "@/components/InicioNosotros/InicioNosotros";
 import { Navbar } from "@/components/Navbar";
+import {QuienesSomos} from "@/components/QuienesSomos/QuienesSomos";
 
 export default function nosotros() {
   return (
       <main>
           <Navbar />
+          <InicioNosotros />
+          <QuienesSomos />
       </main>
   );
 }
